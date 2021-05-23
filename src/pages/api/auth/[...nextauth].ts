eab320cf82fa1e0488c384e43b5b4273e41edf1b
Josimar16 <josimarjr479@gmail.com>
2021-05-23 13:44:46 -0300

    feat: :sparkles: Verificando se o email ja existe
    Caso o email exista ele so busca, caso nao ele cria no fauna db, usando indexes para a busca dos dados

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -14,22 +14,34 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-  jwt: {
-    signingKey: process.env.JWT_SECRET_KEY
-  },
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user;
-
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         );
         return true;
-      } catch {
+      } catch (err) {
         return false;
       }
     },
